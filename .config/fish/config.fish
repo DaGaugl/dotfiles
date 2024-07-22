6a0502b253e6cb707fe570e4bf29ada2e3add860
DaGaugl <81482711+DaGaugl@users.noreply.github.com>
2024-07-22 18:24:21 +0200

    asdf

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,6 +1,6 @@
 set TERM "xterm-256color"
-set EDITOR "emacs -nw"
-set VISUAL "emacsclient -c -a emacs"
+set EDITOR "nvim"
+set VISUAL "nvim"
 fish_vi_key_bindings
 set fish_cursor_default line
 set fish_cursor_insert line
@@ -13,31 +13,22 @@ eval "$(starship init fish)"
 ### CUSTOM STARTUP COMMAND ###
 function fish_greeting
          sysfex
-         configure_pager
 end
 
 ### ALIASES ###
-alias pagman="paru && protonup && exit"
+alias pagman="sudo pacman -Syu && paru && protonup && exit"
 alias ls="exa -la --icons"
 alias cp="cp -iv"
 alias mv="mv -iv"
 alias transde="trans de:en"
 alias trans="trans :de"
 alias urban="udict"
-alias te="emacs -nw"
+alias te="nvim"
 alias :q="exit"
 alias vpn="nordvpn"
-alias monkeytype="typioca"
 alias rm="rm -i"
 alias z="zathura"
+alias lf="yazi"
+alias vim="nvim"
 
-### COLORED MANPAGES ###
-function configure_pager
-    set -gx LESS_TERMCAP_mb \e'[01;31m'       # begin blinking
-    set -gx LESS_TERMCAP_md \e'[01;38;5;74m'  # begin bold
-    set -gx LESS_TERMCAP_me \e'[0m'           # end mode
-    set -gx LESS_TERMCAP_se \e'[0m'           # end standout-mode
-    set -gx LESS_TERMCAP_so \e'[38;5;016m\E[48;5;220m'    # begin standout-mode - info box
-    set -gx LESS_TERMCAP_ue \e'[0m'           # end underline
-    set -gx LESS_TERMCAP_us \e'[04;38;5;146m' # begin underline
-end
+
--- .config/spicetify/Extracted/Themed/xpui/xpui-routes-create-playlist.js
+++ .config/spicetify/Extracted/Themed/xpui/xpui-routes-create-playlist.js
@@ -1,2 +1,2 @@
-"use strict";(("undefined"!=typeof self?self:global).webpackChunkopen=("undefined"!=typeof self?self:global).webpackChunkopen||[]).push([[985],{22304:function(e,n,t){t.r(n),t.d(n,{CreatePlaylist:function(){return s}});var l=t(27378),f=t.n(l),u=t(99038);const s=f().memo((()=>{const{createPlaylist:e}=(0,u.K)();return(0,l.useEffect)((()=>{e()}),[e]),null}));n.default=s}}]);
+"use strict";(("undefined"!=typeof self?self:global).webpackChunkclient_web=("undefined"!=typeof self?self:global).webpackChunkclient_web||[]).push([[4496],{81030:(e,l,n)=>{n.r(l),n.d(l,{default:()=>u});var t=n(30758),f=n(41554);const u=t.memo((function(){var e=(0,f.A)().createPlaylist;return(0,t.useEffect)((function(){e()}),[e]),null}))}}]);
 //# sourceMappingURL=xpui-routes-create-playlist.js.map
